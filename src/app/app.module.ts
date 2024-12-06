ed0ff737052a62b32d57016fd916c0f02f4ba18c
Mitch P. <mitja.pupaher@gmail.com>
2024-12-06 14:08:47 +0100

    Dodana implementacija Firebase-a

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,9 @@ import { HttpClientModule } from '@angular/common/http';
 import { TrgovinaComponent } from './trgovina/trgovina.component';
 import { AboutComponent } from './about/about.component';
 import { PoslovalniceComponent } from './poslovalnice/poslovalnice.component';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getDatabase, provideDatabase } from '@angular/fire/database';
 
 
 @NgModule({
@@ -32,7 +35,11 @@ import { PoslovalniceComponent } from './poslovalnice/poslovalnice.component';
     FormsModule,
     HttpClientModule
   ],
-  providers: [],
+  providers: [
+    provideFirebaseApp(() => initializeApp({"projectId":"rpo-spletna-trgovina","appId":"1:676356888271:web:01d6a08b2fa5eda03bc9ba","storageBucket":"rpo-spletna-trgovina.firebasestorage.app","apiKey":"AIzaSyCAPwqWkHNqpPU6_gm5cGOADPCyhn9o0Wk","authDomain":"rpo-spletna-trgovina.firebaseapp.com","messagingSenderId":"676356888271","measurementId":"G-XST2720TW8"})),
+    provideAuth(() => getAuth()),
+    provideDatabase(() => getDatabase())
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
